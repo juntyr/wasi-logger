69c70d3ec6ec352158fe88d4428c8e6f66a463e6
juntyr <juntyr@users.noreply.github.com>
2024-05-16 09:22:55 +0000

    deploy: b374777af1c3d9a7ad987d018ad708db2377b971

--- search-index.js
+++ search-index.js
@@ -1,5 +1,5 @@
 var searchIndex = new Map(JSON.parse('[\
-["wasi_logger",{"t":"FNNNNNNNNNNN","n":["Logger","borrow","borrow_mut","enabled","flush","from","install","into","log","try_from","try_into","type_id"],"q":[[0,"wasi_logger"],[12,"log"],[13,"core::result"],[14,"core::any"]],"i":[0,1,1,1,1,1,1,1,1,1,1,1],"f":"`{ce{}{}}0{{bd}f}{bh}{cc{}}{{}{{l{bj}}}}4{{bn}h}{c{{l{e}}}{}{}}0{cA`{}}","D":"j","p":[[5,"Logger",0],[5,"Metadata",12],[1,"bool"],[1,"unit"],[5,"SetLoggerError",12],[6,"Result",13],[5,"Record",12],[5,"TypeId",14]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OzAAAAEAAAcAAgACAAMACQADAA=="}]\
+["wasi_logger",{"t":"FNNNNNNNNNNN","n":["Logger","borrow","borrow_mut","enabled","flush","from","install","into","log","try_from","try_into","type_id"],"q":[[0,"wasi_logger"],[12,"log"],[13,"core::result"],[14,"core::any"]],"i":[0,3,3,3,3,3,3,3,3,3,3,3],"f":"`{{{b{c}}}{{b{e}}}{}{}}{{{b{dc}}}{{b{de}}}{}{}}{{{b{f}}{b{h}}}j}{{{b{f}}}l}{cc{}}{{}{{A`{{b{f}}n}}}}{ce{}{}}{{{b{f}}{b{Ab}}}l}{c{{A`{e}}}{}{}}0{{{b{c}}}Ad{}}","D":"j","p":[[1,"reference"],[0,"mut"],[5,"Logger",0],[5,"Metadata",12],[1,"bool"],[1,"unit"],[5,"SetLoggerError",12],[6,"Result",13],[5,"Record",12],[5,"TypeId",14]],"r":[],"b":[],"c":"OjAAAAAAAAA=","e":"OzAAAAEAAAcAAgACAAMACQADAA=="}]\
 ]'));
 if (typeof exports !== 'undefined') exports.searchIndex = searchIndex;
 else if (window.initSearch) window.initSearch(searchIndex);
